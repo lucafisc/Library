cb84a79f3f69ae31681d94e57564439e98749e8f
lucafisc <lucafischer@fastmail.com>
2022-06-10 10:40:46 +0200

    Add default books and fix accordion section

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-// const fontType = []
-
 const colors = [
   "#2f1c3b",
   "#f4cfff",
@@ -46,6 +44,25 @@ class Book {
   }
 }
 
+
+function onLoad() {
+// let default = new Book(inputTitle, inputAuthor, inputUnread);
+// myLibrary.push(default)
+// showBook(myLibrary[myLibrary.length - 1]);
+myLibrary.push(
+new Book ("Made in Tokyo", "Atelier Bow-Wow", false),
+new Book ("Capitalist Realism", "Mark Fisher", true),
+new Book ("Britney Spears: An Unauthorized Biography", "Alix Strauss", true),
+new Book ("Drive My Car", "Haruki Murakami", true),
+new Book ("My Year of Rest and Relaxation", "Otessa Moshfegh", false),
+new Book ("Oryx and Crake", "Margaret Atwood", false)
+)
+for (let i=0; i<myLibrary.length; i++)
+{
+showBook(myLibrary[i])}
+}
+
+
 //add to library
 function addBookToLibrary() {
   let inputTitle = document.getElementById("title").value;
@@ -63,35 +80,19 @@ function addBookToLibrary() {
 
 const bookGridUnread = document.getElementById("book-grid-unread");
 const bookGridRead = document.getElementById("book-grid-read");
-function showBook(newBook) {
-  let newCover = document.createElement("div");
-  newCover.classList.add("cover");
-  newCover.style.backgroundColor = colors[randomArrayIndex(colors)];
-  let bookDetails = document.createElement("div");
-  bookDetails.classList.add("book-details");
-  do {
-    bookDetails.style.color = colors[randomArrayIndex(colors)];
-  } while (newCover.style.backgroundColor === bookDetails.style.color);
-  let newTitle = document.createElement("h1");
-  newTitle.classList.add("title");
-  newTitle.textContent = newBook.title;
-  let newAuthor = document.createElement("h2");
-  newAuthor.classList.add("author");
-  newAuthor.classList.add("hr");
-  newAuthor.textContent = newBook.author;
-  let coverArt = document.createElement("i");
-  coverArt.classList.add("cover-art");
-  coverArt.setAttribute("data-feather", icons[randomArrayIndex(icons)]);
-  coverArt.style.color = bookDetails.style.color;
-  let tools = document.createElement("div");
-  tools.classList.add("tools");
-  let newEdit = newEditBtn(newBook, bookDetails);
-  let newDelete = document.createElement("i");
-  newDelete.classList.add("tool");
-  newDelete.setAttribute("data-feather", "trash-2");
-  newDelete.setAttribute("id", "del");
-  newDelete.style.color = bookDetails.style.color;
 
+//combine all functions to create new book
+function showBook(newBook) {
+  let newCover = newCoverDiv();
+  let bookDetails = newBookDetailsDiv(newCover);
+  let newTitle = newTitleText(newBook);
+  let newAuthor = newAuthorText(newBook);
+  let coverArt = newCoverArt(bookDetails);
+  let tools = newToolsDiv();
+  let newEdit = newEditBtn(newBook, bookDetails);
+  let newDelete = newDeleteBtn(bookDetails);
+
+  //append all new elements
   bookDetails.appendChild(newTitle);
   bookDetails.appendChild(newAuthor);
   newCover.appendChild(bookDetails);
@@ -100,15 +101,83 @@ function showBook(newBook) {
   tools.appendChild(newEdit);
   newCover.appendChild(tools);
 
-  newCover.setAttribute("data-index", `${newBook.title} - ${newBook.author}`); //add unique data-index for cover div
+  //add unique data-index for cover div
+  newCover.setAttribute("data-index", `${newBook.title} - ${newBook.author}`); 
+  
+  //insert in corresponding section
   if (newBook.unread) {
     bookGridUnread.insertBefore(newCover, bookGridUnread.firstElementChild);
+    bookGridUnread.style.maxHeight = "fit-content";
+
   } else {
     bookGridRead.insertBefore(newCover, bookGridRead.firstElementChild);
+    bookGridRead.style.maxHeight = "fit-content";
   }
   feather.replace();
 }
 
+//create new cover div with random color
+function newCoverDiv() {
+    let newCover = document.createElement("div");
+    newCover.classList.add("cover");
+    newCover.style.backgroundColor = colors[randomArrayIndex(colors)];
+    return newCover;
+}
+
+//create new book details div with different color from cover
+function newBookDetailsDiv(newCover) {
+    let bookDetails = document.createElement("div");
+    bookDetails.classList.add("book-details");
+    do {
+        bookDetails.style.color = colors[randomArrayIndex(colors)];
+    } while (newCover.style.backgroundColor === bookDetails.style.color);
+    return bookDetails;
+}
+
+//create new title text element
+function newTitleText(newBook) {
+    let newTitle = document.createElement("h1");
+    newTitle.classList.add("title");
+    newTitle.textContent = newBook.title;
+    return newTitle;
+}
+
+//create new author text element
+function newAuthorText(newBook) {
+    let newAuthor = document.createElement("h2");
+    newAuthor.classList.add("author");
+    newAuthor.classList.add("hr");
+    newAuthor.textContent = newBook.author;
+    return newAuthor;
+}
+
+//create tools div
+function newToolsDiv() {
+    let tools = document.createElement("div");
+    tools.classList.add("tools");
+    return tools;
+}
+
+//create cover art
+function newCoverArt(bookDetails) {
+    let coverArt = document.createElement("i");
+    coverArt.classList.add("cover-art");
+    coverArt.setAttribute("data-feather", icons[randomArrayIndex(icons)]);
+    coverArt.style.color = bookDetails.style.color;
+    return coverArt;
+}
+
+//create delete btn
+function newDeleteBtn(bookDetails) {
+    let newDelete = document.createElement("i");
+    newDelete.classList.add("tool");
+    newDelete.setAttribute("data-feather", "trash-2");
+    newDelete.setAttribute("id", "del");
+    newDelete.style.color = bookDetails.style.color;
+    return newDelete;
+}
+
+//create edit btn
 function newEditBtn(newBook, bookDetails) {
     let newEdit = document.createElement("i");
     newEdit.classList.add("tool");
@@ -152,11 +221,22 @@ function toggleForm() {
 }
 
 //collapse section button
-const collapse = document.getElementById("collapse");
-collapse.addEventListener("click", () => {
-  collapse.classList.toggle("btn-rotate");
-  bookGridUnread.classList.toggle("hidden");
-});
+const accSection = document.getElementsByClassName("accordion");
+let x;
+for (x=0; x<accSection.length; x++) {
+    accSection[x].addEventListener("click", function() {
+        this.classList.toggle("collapse");
+        let section = this.nextElementSibling;
+        console.log(section)
+        if (section.style.maxHeight) {
+            section.style.maxHeight = null;
+        }
+        else {
+            section.style.maxHeight = section.scrollHeight + "px";
+        }
+    });
+}
+
 
 //delete book
 const content = document.getElementById("content");
@@ -185,18 +265,30 @@ content.addEventListener("click", function (e) {
 
     switch (myLibrary[i].unread) {
       case true:
-        myLibrary[i].unread = false;
         bookGridRead.appendChild(e.target.parentNode.parentNode);
+        bookGridRead.style.maxHeight = "fit-content";
+        myLibrary[i].unread = false;
         break;
       case false:
-        myLibrary[i].unread = true;
         bookGridUnread.appendChild(e.target.parentNode.parentNode);
+        bookGridUnread.style.maxHeight = "fit-content";
+        myLibrary[i].unread = true;
         break;
     }
     
+    //replace edit button
     let newEdit = newEditBtn(myLibrary[i], e.target.parentNode.firstChild);
     e.target.parentNode.appendChild(newEdit);
     e.target.remove();
     feather.replace();
   }
 });
+
+
+//todo
+//prevent duplicated books
+//grid in mobile
+//hover menu on icons
+
+
+document.onload = onLoad()
